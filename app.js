460f8dea785c054625cefc9117409b9ec094fdb7
mariagimenezbustos <134734638+mariagimenezbustos@users.noreply.github.com>
2023-08-08 17:05:19 +0200

    GET capitals, by id, by language, PUT population

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,6 +17,6 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
+// app.use('/api/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
+});
+
+/* GET all capitals */
+router.get("/capitals", async (req, res) => {
+  try {
+    const results = await db("SELECT * FROM capital ORDER BY id ASC;");
+    res.send(results.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* GET capital by id */
+router.get("/capitals/cities/:capital_id", async (req, res) => {
+  const id = req.params.capital_id;
+
+  try {
+    const results = await db(`SELECT * FROM capital WHERE id = ${id};`);
+    res.send(results.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* GET capitals by language */
+router.get("/capitals/:language", async (req, res) => {
+  const language = req.params.language;
+
+  try {
+    const results = await db(`SELECT * FROM capital WHERE language LIKE "%${language}%";`);
+    res.send(results.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* PUT population by id */
+router.put("/capitals/cities/:capital_id", async (req, res) => {
+  const id = req.params.capital_id;
+  const { population } = req.body;
+  console.log("REQ.BODY", req.body);
+
+  try {
+    await db(`UPDATE capital SET population = ${population} WHERE id = ${id};`);
+    const results = await db("SELECT * FROM capital;");
+    res.send(results.data);
+  } catch (error) {
+    console.log("something not working", error);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
